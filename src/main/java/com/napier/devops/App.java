88a53989c4fde80d48d5f33c4f2e61224a4021e2
nkoik <nikos@koikas.eu>
2024-10-02 00:22:08 +0300

    first implementation for extracting all countries

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -1,8 +1,40 @@
 package com.napier.devops;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class App {
+     /**
+     * Connection to MySQL database.
+     */
     public static void main(String[] args)
     {
-        System.out.println("Boo yah!");
+        String databaseUrl = args.length < 1 ? "localhost:33060" : args[0];
+        try {
+            Connection con = DBconnector.connect(databaseUrl, 10000);
+            if (con == null) return;
+            Reports report = new Reports();
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String sql = "select * from country order by population desc";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(sql);
+
+            report.extract(rset, "country", new String[]{
+                    ReportColumns.Countries.Code.toString(),
+                    ReportColumns.Countries.Name.toString(),
+                    ReportColumns.Countries.Continent.toString(),
+                    ReportColumns.Countries.Region.toString(),
+                    ReportColumns.Countries.Population.toString(),
+                    ReportColumns.Countries.Capital.toString()
+            });
+            DBconnector.disconnect();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
+
 }
