d34370b290e90ff89c7298547cc2a45ffb58eb9c
nkoik <nikos@koikas.eu>
2024-10-02 15:44:31 +0300

    remove code and files

--- src/main/java/com/napier/devops/ReportColumns.java
+++
@@ -1,8 +0,0 @@
-package com.napier.devops;
-
-public class ReportColumns {
-    protected enum Countries {
-        Code, Name, Continent, Population, Capital, Region
-    }
-
-}
--- src/main/java/com/napier/devops/Reports.java
+++ src/main/java/com/napier/devops/Reports.java
@@ -8,17 +8,27 @@ import java.sql.ResultSet;
 
 
 public class Reports {
+    // Column enums that can be used in the reports
+    public enum Columns {
+        Code, Name, Continent, Population, Capital, Region
+    }
+    /**
+     * Extract the results to a .csv file.
+     * @param rset The ResultSet of the query from db.
+     * @param fileName The file name.
+     * @param cols The columns that should be extracted.
+     */
     public void extract(ResultSet rset, String fileName, String[] cols) throws IOException {
         StringBuilder sb = new StringBuilder();
         try {
             for (String col : cols) {
-                sb.append(col).append("\t");
+                sb.append(col).append(",");
             }
-            sb.append("\r\n");
+            sb.append("\r\n\n");
             //cycle
             while (rset.next()) {
                 for (int i = 0; i < cols.length; i++) {
-                    sb.append(rset.getString(cols[i])).append("\t");
+                    sb.append(rset.getString(cols[i])).append(",");
                     if (i == cols.length - 1) {
                         sb.append("\r\n");
                     }
@@ -26,21 +36,13 @@ public class Reports {
             }
             new File("./tmp/").mkdir();
             BufferedWriter writer = new BufferedWriter(
-                    new FileWriter(new File("./tmp/" + fileName + ".txt")));
+                    new FileWriter("./tmp/" + fileName + ".csv"));
             writer.write(sb.toString());
             writer.close();
             System.out.println(sb.toString());
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Failed to get details");
-            return;
         }
-        // write to file
-        new File("./output/").mkdir();
-        BufferedWriter writer = new BufferedWriter(
-                new FileWriter(new File("./tmp/" + fileName + ".txt")));
-        writer.write(sb.toString());
-        writer.close();
-        System.out.println(sb.toString());
     }
 }
