e77dc721e986a565dcd5ddd06f2f7423b775e3d0
Nikos Koikas <24964750+nkoik@users.noreply.github.com>
2024-10-05 16:28:53 +0300

    Change the format of the reports & error handling was added (#44)
    * change report csv
    
    * change report csv localhost
    
    * change db
    
    * error handling from reports
    
    * change linting

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ COPY ./target/devops.jar /tmp
 WORKDIR /tmp
 # change last parameter to 10000 if running on github actions
 #if running locally and database aleady started delay can be zero
-ENTRYPOINT ["java", "-jar", "devops.jar", "world:3306", "10000"]
+ENTRYPOINT ["java", "-jar", "devops.jar", "db:3306", "10000"]
--- src/main/java/com/napier/devops/helpers/IUserSelectionProcessor.java
+++ src/main/java/com/napier/devops/helpers/IUserSelectionProcessor.java
@@ -2,9 +2,9 @@ package com.napier.devops.helpers;
 
 import com.napier.devops.Reports;
 
+import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public interface IUserSelectionProcessor {
-    void processUserSelection(Reports report, Statement stmt, Integer userInput) throws SQLException;
+    void processUserSelection(Reports report, Connection con, Integer userInput) throws SQLException;
 }
