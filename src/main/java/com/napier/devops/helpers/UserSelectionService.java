8f433d0b66039a8a325a53e627f236f19a696172
Nikos <24964750+nkoik@users.noreply.github.com>
2024-11-09 17:30:00 +0200

    population total country/region/continent/city/district/world report

--- src/main/java/com/napier/devops/helpers/UserSelectionService.java
+++ src/main/java/com/napier/devops/helpers/UserSelectionService.java
@@ -1,9 +1,7 @@
 package com.napier.devops.helpers;
 
 import com.napier.devops.Reports;
-import com.napier.devops.reports.CapitalCitiesList;
-import com.napier.devops.reports.CountriesList;
-import com.napier.devops.reports.CitiesList;
+import com.napier.devops.reports.*;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -39,6 +37,16 @@ public class UserSelectionService {
         processors.put("20", new CapitalCitiesList( "TopCapitalCitiesPerPopulation", QueryConstants.CITY_CAPITALS_POPULATION_DESC));
         processors.put("21", new CapitalCitiesList( "CapitalCitiesInContinentWithLimit", QueryConstants.CITY_CAPITALS_CONTINENT_POPULATION_DESC));
         processors.put("22", new CapitalCitiesList( "CapitalCitiesInRegionWithLimit", QueryConstants.CITY_CAPITALS_REGION_POPULATION_DESC));
+        processors.put("23", new PopulationInCitiesList( "PopulationLivingNotLivingInCitiesPerContinent", QueryConstants.CONTINENT_LIVING_NOTLIVING_IN_CITY_DESC));
+        processors.put("24", new PopulationInCitiesList("PopulationLivingNotLivingInCitiesPerRegion", QueryConstants.REGION_LIVING_NOTLIVING_IN_CITY_DESC));
+        processors.put("25", new PopulationInCitiesList("PopulationLivingNotLivingInCitiesPerCountry", QueryConstants.COUNTRY_LIVING_NOTLIVING_IN_CITY_DESC));
+        processors.put("26", new PopulationList("TotalPopulationInWorld", QueryConstants.TOTAL_POPULATION_WORLD));
+        processors.put("27", new PopulationList("TotalPopulationInContinent", QueryConstants.TOTAL_POPULATION_CONTINENT));
+        processors.put("28", new PopulationList("TotalPopulationInRegion", QueryConstants.TOTAL_POPULATION_REGION));
+        processors.put("29", new PopulationList("TotalPopulationInCountry", QueryConstants.TOTAL_POPULATION_COUNTRY));
+        processors.put("30", new PopulationList("TotalPopulationInDistrict", QueryConstants.TOTAL_POPULATION_DISTRICT));
+        processors.put("31", new PopulationList("TotalPopulationInCity", QueryConstants.TOTAL_POPULATION_CITY));
+        processors.put("32", new LanguageWorldList("LanguageWorldPerPopulation", QueryConstants.LANGUAGE_PERCENTAGE_WORLD));
     }
 
     /**
@@ -70,10 +78,10 @@ public class UserSelectionService {
      */
     public Map<String, String> getUserInput() {
         // Set of question IDs that require extra user input
-        final Set<String> questionsWithoutAnyUserInput = Set.of("1", "7", "17", "20","26");
-        final Set<String> questionsWithLimit = Set.of("4","5","6","12","13","14","15","16","21","22","23");
-        final Set<String> questionContinent = Set.of("2","5","8","13", "18", "21","27");
-        final Map<String, String> categories = new HashMap<String, String>() {
+        final Set<String> questionsWithoutAnyUserInput = Set.of("1", "7", "17", "20","23","24","25","26", "32");
+        final Set<String> questionsWithLimit = Set.of("4","5","6","12","13","14","15","16","20", "21","22");
+        final Set<String> questionsForContinentCategory = Set.of("2","5","8","13", "18", "21","27");
+        final Map<String, String> categories = new HashMap<>() {
             {
                 put("3", "region");
                 put("6", "region");
@@ -88,7 +96,8 @@ public class UserSelectionService {
                 put("29", "country");
                 put("30", "district");
                 put("31", "city");
-            }};
+            }
+        };
         String questionSelected = "";
         String userInput = "";
         String limit = "";
@@ -110,13 +119,23 @@ public class UserSelectionService {
         System.out.println("20. The top N populated capital cities in the world where N is provided by the user.");
         System.out.println("21. The top N populated capital cities in a continent where N is provided by the user.");
         System.out.println("22. The top N populated capital cities in a region where N is provided by the user.");
+        System.out.println("23. The population of people, people living in cities, and people not living in cities in each continent.");
+        System.out.println("24. The population of people, people living in cities, and people not living in cities in each region.");
+        System.out.println("25. The population of people, people living in cities, and people not living in cities in each country.");
+        System.out.println("26. The population of the world.");
+        System.out.println("27. The population of a continent.");
+        System.out.println("28. The population of a region.");
+        System.out.println("29. The population of a country.");
+        System.out.println("30. The population of a district.");
+        System.out.println("31. The population of a city.");
+        System.out.println("32. The number of people who speak 'Chinese', 'English', 'Hindi', 'Spanish', 'Arabic' from greatest number to smallest, including the percentage of the world population.");
 
         System.out.println("\n");
 
         try {
             questionSelected = read_range(keyboard, 1, 32);
             if (!questionsWithoutAnyUserInput.contains(questionSelected)) {
-                if(questionContinent.contains(questionSelected)) {
+                if(questionsForContinentCategory.contains(questionSelected)) {
                     userInput = getContinent();
                 } else {
                     userInput = getInput(categories.get(questionSelected));
@@ -173,8 +192,8 @@ public class UserSelectionService {
         String[] continents = {"Asia", "Africa", "Europe", "North America", "South America", "Oceania", "Antarctica"};
         System.out.println("Please specify the number of the continent you're interested in:");
         Scanner keyboard = new Scanner(System.in);
-        for (int i = 0; i < continents.length; i++) System.out.println((i + 1) + ": " + continents[i]);
-        return continents[Integer.parseInt(read_range(keyboard, 1, 7))];
+        for (int i = 0; i < continents.length; i++) System.out.println((i+1) + ": " + continents[i]);
+        return continents[Integer.parseInt(read_range(keyboard, 1, 7))-1];
     }
 
     /**
@@ -206,9 +225,8 @@ public class UserSelectionService {
      * @return The user-provided input.
      */
     private static String getInput (String category) {
-        String result = "";
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please specify the " + category + " you want: ");
-        return scanner.next();
+        System.out.print("Enter the name of the " + category + " you want to retrieve data for: ");
+        return scanner.nextLine();
     }
 }
