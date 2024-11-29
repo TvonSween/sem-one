822486711782a5ec614d770976b71e3d18bc02ac
Tess Vaughan <tess.vaughan.2@glasgow.ac.uk>
2024-11-29 14:35:08 +0000

    updated github actions for Integration tests

---
+++ src/test/java/com/napier/devops/AppIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.napier.devops;
+
+import com.napier.devops.helpers.UserSelectionService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppIntegrationTest
+{
+    public static DBconnector dbConnector;
+    public static Connection con = null;
+
+    @BeforeAll
+    static void init()
+    {
+        dbConnector = new DBconnector();
+        // if running locally delay can be zero if database is running
+        // To work on GitHub Actions set the delay to 10000 as the database takes a while to build
+        con = dbConnector.connect("localhost:33060", 10000);
+
+    }
+
+    @Test
+    void testProcessUserSelection()
+    {
+
+        UserSelectionService userSelectionService = new UserSelectionService();
+        Reports report = new Reports();
+
+        assertEquals("North America", userSelectionService.processUserSelection("2", report, con, "North America", null));
+        System.out.println("successfully retrieved " + "North America");
+    }
+}
